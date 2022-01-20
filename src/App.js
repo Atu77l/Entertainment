1c9633fc13d9c4a88a4196e6cfe4d54082d7bbd8
Atul Kesharwani <kesharwaniatul9935@gmail.com>
2022-01-20 23:42:32 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React,{ useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';import {NavLink as Link} from 'react-router-dom';
+import { BsPlusLg } from "react-icons/bs";
+import { BsXLg } from "react-icons/bs";
+import './App.css';
+import Calculator from './Calculator/Calculator';
+import Notes from './Notes/Notes';
+import Navbar from './Navbar/Navbar';
+import Footer from'./Footer/Footer';
+
+
+
+const App = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+  
+  var time1=   new Date().toLocaleTimeString();
+
+  const [ctime,setTime] = useState(time1);
+
+  const Update = () => {
+    time1 = new  Date().toLocaleTimeString();
+    setTime(time1);
+  };
+
+  setInterval(Update,1000);
+  return(
+    <>
+    <h1 className="header1">Welcome to Atul Kesharwani Website</h1>
+    <h2 className="time"> {ctime}</h2>
+    <Router>
+       <Navbar />
+      <Switch>
+        <Route exact path='/'  component={Calculator} />
+        <Route path='/calculator' component={Calculator} />
+        <Route path='/notes' component={Notes} />
+      </Switch>
+    </Router>
+    <Footer/>
+    </>
+  );
+};
+export default App;
